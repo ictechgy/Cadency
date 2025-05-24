610b3a1616d46146331e42d08b7d4d6604315597
Coden <ictechgy@gmail.com>
2025-05-24 22:51:29 +0900

    [feat] 프로젝트 시작

---
+++ Cadency Watch App/CadencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CadencyApp.swift
+//  Cadency Watch App
+//
+//  Created by JINHONG AN on 5/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Cadency_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cadency Watch AppTests/Cadency_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Cadency_Watch_AppTests.swift
+//  Cadency Watch AppTests
+//
+//  Created by JINHONG AN on 5/24/25.
+//
+
+import Testing
+@testable import Cadency_Watch_App
+
+struct Cadency_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
