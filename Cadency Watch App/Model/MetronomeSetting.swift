96f3a8b0cca1cabfc79a08c8afb796e1d21c85ef
Coden <ictechgy@gmail.com>
2025-05-28 02:44:47 +0900

    [refactor] BPM 및 HapticType 기본값 정의

--- Cadency Watch App/Model/MetronomeSetting.swift
+++ Cadency Watch App/Model/MetronomeSetting.swift
@@ -12,13 +12,23 @@ import WatchKit
 final class MetronomeSetting {
     var bpm: Int
     var hapticTypeRawValue: Int
-
+    
     init(bpm: Int, hapticType: WKHapticType) {
         self.bpm = bpm
         self.hapticTypeRawValue = hapticType.rawValue
     }
+}
 
+extension MetronomeSetting {
     var hapticType: WKHapticType? {
         return WKHapticType(rawValue: hapticTypeRawValue)
+    }
+    
+    static var defaultBPM: Int {
+        return 180
+    }
+    
+    static var defaultHapticType: WKHapticType {
+        return .directionUp
     }
 }
