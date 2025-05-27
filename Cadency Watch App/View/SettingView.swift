f7e4825f87e5446252995ff35d065bbf4c186321
Coden <ictechgy@gmail.com>
2025-05-28 02:54:29 +0900

    [refactor] 설정화면 수정 중

--- Cadency Watch App/View/SettingView.swift
+++ Cadency Watch App/View/SettingView.swift
@@ -6,15 +6,15 @@
 //
 
 import SwiftUI
-import Combine
+import SwiftData
 
 struct SettingView: View {
     // 상태값 선언
-    private let bpmRange: ClosedRange<Int> = 60...240
-    @State private var bpm: Double = 180
-    @State private var isTriggered: Bool = false
-    @State private var hapticStrength: WKHapticType = .directionUp
-    @State private var timerCancellable: AnyCancellable?
+    @Environment(\.modelContext) private var modelContext
+    @Query private var metronomeSettings: [MetronomeSetting]
+    
+    // 설정값 불러와서 State bpm에 연결
+    private let bpmRange: ClosedRange<Int> = 140...240
     @FocusState private var bpmPickerFocused: Bool
     // SwiftData로 저장 필요 - BPM 및 진동세기값
 
@@ -26,11 +26,6 @@ struct SettingView: View {
     ]
     @State private var hapticIndex: Int = 0
 
-    // BPM에서 interval(초) 계산
-    private var interval: Double {
-        60.0 / Double(bpm)
-    }
-
     // 페이징 형태로 좌측에는 시작/정지만, 우측으로 페이징하면 상세설정할 수 있도록 변경
     // 백그라운드로 가거나 화면이 꺼진 경우 진동 동작은 불가할 수 있으므로 이에 대한 대응책 필요 - 소리, 화면, 진동
     // 화면으로 BPM 노티하는 기능도 별도로 있으면 괜찮으려나? BPM 맞춰서 반짝반짝.
@@ -39,6 +34,7 @@ struct SettingView: View {
         VStack(spacing: 16) {
             // BPM 표시 및 크라운 조작
             VStack {
+                
                 Text("BPM")
                     .font(.caption2)
                 Picker(selection: $bpm, label: Text("\(Int(bpm))")) {
@@ -51,7 +47,7 @@ struct SettingView: View {
                 .focusable()
                 // FIXME: 뷰 동작 보고 digitalCrownRotation 연결 필요
             }
-
+            
             // 진동 세기 선택
             VStack {
                 Picker(selection: $hapticIndex) {
@@ -64,35 +60,7 @@ struct SettingView: View {
                 }
                 .pickerStyle(.navigationLink)
             }
-
-            // 시작/정지 버튼
-            Button(action: {
-                isTriggered ? stopMetronome() : startMetronome()
-            }) {
-                Text(isTriggered ? "정지" : "시작")
-                    .font(.title3)
-                    .frame(maxWidth: .infinity)
-            }
-            .tint(isTriggered ? .red : .green)
-            .buttonStyle(.borderedProminent)
         }
         .padding()
-    }
-
-    // 메트로놈 시작 함수
-    func startMetronome() {
-        isTriggered = true
-        timerCancellable = Timer.publish(every: interval, on: .main, in: .common)
-            .autoconnect()
-            .sink { _ in
-                WKInterfaceDevice.current().play(hapticOptions[hapticIndex].type)
-            }
-    }
-
-    // 메트로놈 정지 함수
-    func stopMetronome() {
-        isTriggered = false
-        timerCancellable?.cancel()
-        timerCancellable = nil
     }
 }
