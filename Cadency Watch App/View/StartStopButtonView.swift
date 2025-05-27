696f4774cb3c317bbd1c0b01370e43089dda30c3
Coden <ictechgy@gmail.com>
2025-05-28 02:49:08 +0900

    [refactor] 시작/정지버튼 이관 및 데이터 fetch부 구현

--- Cadency Watch App/View/StartStopButtonView.swift
+++ Cadency Watch App/View/StartStopButtonView.swift
@@ -6,9 +6,60 @@
 //
 
 import SwiftUI
+import Combine
+import SwiftData
 
 struct StartStopButtonView: View {
-    var body: some View {
-        Text("Start/Stop")
+    @State private var isTriggered: Bool = false
+    @State private var timerCancellable: AnyCancellable?
+    @Query private var metronomeSettings: [MetronomeSetting]
+    
+    private var bpm: Int {
+        metronomeSettings.last?.bpm ?? MetronomeSetting.defaultBPM
     }
+    
+    private var haptickType: WKHapticType {
+        metronomeSettings.last?.hapticType ?? MetronomeSetting.defaultHapticType
+    }
+    
+    // BPM에서 interval(초) 계산
+    private var interval: Double {
+        60.0 / Double(bpm)
+    }
+    
+    var body: some View {
+        // 시작/정지 버튼
+        Button {
+            isTriggered ? stopMetronome() : startMetronome()
+        } label: {
+            Text(isTriggered ? "정지" : "시작")
+                .font(.title3)
+                .frame(maxWidth: .infinity)
+        }
+        .tint(isTriggered ? .red : .green)
+        .buttonStyle(.borderedProminent)
+    }
+}
+
+extension StartStopButtonView {
+    // 메트로놈 시작 함수
+    func startMetronome() {
+        isTriggered = true
+        timerCancellable = Timer.publish(every: interval, on: .main, in: .common)
+            .autoconnect()
+            .sink { _ in
+                WKInterfaceDevice.current().play(haptickType)
+            }
+    }
+
+    // 메트로놈 정지 함수
+    func stopMetronome() {
+        isTriggered = false
+        timerCancellable?.cancel()
+        timerCancellable = nil
+    }
+}
+
+#Preview {
+    StartStopButtonView()
 }
