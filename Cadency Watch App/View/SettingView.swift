f602e97e1dc3dfae07ced1266f9df67060a26446
Coden <ictechgy@gmail.com>
2025-05-30 13:22:26 +0900

    [feat] BPM 변경코드 ViewModel로 이관중

--- Cadency Watch App/View/SettingView.swift
+++ Cadency Watch App/View/SettingView.swift
@@ -10,24 +10,32 @@ import SwiftData
 
 struct SettingView: View {
     @Environment(\.modelContext) private var modelContext
-    @Query private var metronomeSettings: [MetronomeSetting]
+    let viewModel: SettingViewModel
     
-    @State private var bpm: Double = 0
-    private let bpmStep: Double = 5
-    @State private var hapticIndex: Int = 0
     @FocusState private var bpmPickerFocused: Bool
     // SwiftData로 저장 필요 - BPM 및 진동세기값
     // 백그라운드로 가거나 화면이 꺼진 경우 진동 동작은 불가할 수 있으므로 이에 대한 대응책 필요 - 소리, 화면, 진동
     // 화면으로 BPM 노티하는 기능도 별도로 있으면 괜찮으려나? BPM 맞춰서 반짝반짝.
     // 더불어 > 조금 느려요 / 조금 빨라요 노티기능..?
+    private var bpmRange: ClosedRange<Int> {
+        SettingViewModel.Constants.bpmRange
+    }
+    
+    private var bpmStep: Double {
+        SettingViewModel.Constants.bpmStep
+    }
+    
     var body: some View {
         VStack(spacing: 16) {
             // BPM 표시 및 크라운 조작
             VStack {
-                
                 Text("BPM")
                     .font(.caption2)
-                Picker(selection: $bpm, label: EmptyView()) {
+                Picker(selection: Binding(get: {
+                    viewModel.bpm
+                }, set: { newBPM in
+                    viewModel.changeBPM(to: newBPM)
+                }), label: EmptyView()) {
                     ForEach(bpmRange, id: \.self) { value in
                         Text("\(value)")
                             .tag(value)
@@ -36,7 +44,11 @@ struct SettingView: View {
                 .pickerStyle(.wheel)
                 .focusable()
                 .digitalCrownRotation(
-                    $bpm,
+                    Binding(get: {
+                        viewModel.bpm
+                    }, set: { newBPM in
+                        viewModel.changeBPM(to: newBPM)
+                    }),
                     from: Double(bpmRange.lowerBound),
                     through: Double(bpmRange.upperBound),
                     by: bpmStep,
--- Cadency Watch App/View/SettingViewModel.swift
+++ Cadency Watch App/View/SettingViewModel.swift
@@ -17,10 +17,24 @@ final class SettingViewModel {
             ("Medium", .directionUp),
             ("Strong", .retry) // TODO: 대체 필요
         ]
+        static let bpmStep: Double = 5
     }
     
     private(set) var bpm: Double
     private(set) var hapticType: WKHapticType
+    private var hapticIndex: Int = 0
     private let modelContext: ModelContext
     
+    init(modelContext: ModelContext) {
+        modelContext.fetch(., batchSize: <#T##Int#>)
+        self.bpm = bpm
+        self.hapticType = hapticType
+        self.modelContext = modelContext
+    }
+}
+
+extension SettingViewModel {
+    func changeBPM(to newBPM: Double) {
+        
+    }
 }
