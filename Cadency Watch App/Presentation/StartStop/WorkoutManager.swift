d41d61e02d57bb127e93f1b4c2e6b1b64e6bcdd9
Coden <ictechgy@gmail.com>
2025-10-11 19:54:30 +0900

    [refactor] 불필요 권한 사용부 제거

--- Cadency Watch App/Presentation/StartStop/WorkoutManager.swift
+++ Cadency Watch App/Presentation/StartStop/WorkoutManager.swift
@@ -10,18 +10,6 @@ import HealthKit
 final class WorkoutManager {
     private let store = HKHealthStore()
     private var session: HKWorkoutSession?
-    private var builder: HKLiveWorkoutBuilder? // TODO: 판단 후 제거 예정
-
-    // TODO: 판단 후 제거 예정
-    func requestAuthorization() async throws {
-        let toShare: Set = [HKObjectType.workoutType()]
-        let toRead = [
-            HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning),
-            HKQuantityType.quantityType(forIdentifier: .runningSpeed) // 유용한 보조지표
-        ].compactMap { $0 }
-        
-        try await store.requestAuthorization(toShare: toShare, read: Set(toRead))
-    }
 
     func startWorkout() throws {
         let config = HKWorkoutConfiguration()
@@ -29,22 +17,14 @@ final class WorkoutManager {
         config.locationType = .unknown
 
         session = try HKWorkoutSession(healthStore: store, configuration: config)
-        builder = session?.associatedWorkoutBuilder()
-        builder?.dataSource = HKLiveWorkoutDataSource(healthStore: store, workoutConfiguration: config)
-
         session?.startActivity(with: Date())
-        builder?.beginCollection(withStart: Date()) { _, _ in }
     }
     
-    // TODO: 미종료 상태에서 사용하도록 수정 예정 
     func pauseWorkout() {
         session?.pause()
     }
 
     func stopWorkout() {
         session?.end()
-        builder?.endCollection(withEnd: Date()) { _, _ in
-            self.builder?.finishWorkout { _, _ in }
-        }
     }
 }
